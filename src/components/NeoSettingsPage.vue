37746b6f4986a2746cdae0b0c12563f4e7b757d9
SniperGER <janikschmidt@icloud.com>
2019-03-02 09:47:24 +0100

    More work on the Settings template (festivaledu/neo#38)

--- src/components/NeoSettingsPage.vue
+++ src/components/NeoSettingsPage.vue
@@ -2,7 +2,7 @@
 	<div class="page" data-page-id="settings">
 		<metro-list-view class="fixed-width" acrylic="acrylic-80" menuTitle="Settings" ref="settingsView">
 			<template slot="actions">
-				<metro-list-view-action icon="more" />
+				<metro-list-view-action icon="add" @click.native="addGroup()" />
 			</template>
 			
 			<template slot="list-items">
@@ -10,12 +10,99 @@
 				<metro-list-view-menu-item class="single-line" title="Server" page="server_settings" />
 				
 				<metro-list-view-menu-separator title="Groups" />
-				<metro-list-view-menu-item class="single-line" title="%group_name%" />
+				<metro-list-view-menu-item class="single-line" title="%group_name%" page="group_settings" />
 			</template>
 			
 			<template slot="pages">
 				<div class="page" data-page-id="server_settings" data-page-title="Server">
-					<p>test</p>
+					<metro-toggle-switch itemHeader="%setting_name%" />
+				</div>
+				
+				<div class="page" data-page-id="group_settings" data-page-title="%group_name%">
+					<h3>Actions</h3>
+					<button>Delete Group</button>
+					
+					<h3>Permissions</h3>
+					<div class="row">
+						<div class="col col-6"></div>
+						<div class="col col-2 text-center">
+							<h5>Granted</h5>
+						</div>
+						<div class="col col-2 text-center">
+							<h5>Inherited</h5>
+						</div>
+						<div class="col col-2 text-center">
+							<h5>Denied</h5>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col col-6">
+							<p class="text-label">Read Messages</p>
+							<p class="detail-text-label">neo.global.read</p>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo1_radio1" name="demo1_radio" value="permit" v-model="demoPermission">
+								<label for="demo1_radio1" />
+							</div>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo1_radio2" name="demo1_radio" value="inherit" v-model="demoPermission">
+								<label for="demo1_radio2" />
+							</div>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo1_radio3" name="demo1_radio" value="deny" v-model="demoPermission">
+								<label for="demo1_radio3" />
+							</div>
+						</div>
+					</div>
+					
+					<p>{{demoPermission}}</p>
+					
+					<h3>Plugin Permissions</h3>
+					<div class="row">
+						<div class="col col-6"></div>
+						<div class="col col-2 text-center">
+							<h5>Granted</h5>
+						</div>
+						<div class="col col-2 text-center">
+							<h5>Inherited</h5>
+						</div>
+						<div class="col col-2 text-center">
+							<h5>Denied</h5>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col col-6">
+							<p class="text-label">Plugin 1: %permission%</p>
+							<p class="detail-text-label">neo.plugin1.permission</p>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo2_radio1" name="demo2_radio" value="permit" v-model="demoPermission2">
+								<label for="demo2_radio1" />
+							</div>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo2_radio2" name="demo2_radio" value="inherit" v-model="demoPermission2">
+								<label for="demo2_radio2" />
+							</div>
+						</div>
+						<div class="col col-2 text-center">
+							<div class="radio">
+								<input type="radio" id="demo2_radio3" name="demo2_radio" value="deny" v-model="demoPermission2">
+								<label for="demo2_radio3" />
+							</div>
+						</div>
+					</div>
+					
+					<p>{{demoPermission2}}</p>
 				</div>
 			</template>
 		</metro-list-view>
@@ -23,6 +110,37 @@
 </template>
 
 <style lang="less">
+.page[data-page-id="settings"] {
+	.frame-header {
+		right: 320px;
+		
+		p.title {
+			font-size: 32px;
+			height: 64px;
+		}
+	}
+	
+	h3:not(:first-of-type) {
+		margin-top: 30px;
+	}
+	
+	.col .radio {
+		margin: 11px 0;
+		
+		label {
+			padding-left: 20px;
+		}	
+	}
+	
+	.text-label {
+		font-weight: 600;
+	}
+	
+	.detail-text-label {
+		color: var(--base-medium)
+	}
+}
+
 .list-view-item.single-line {
 	span.text-label {
 		font-weight: 400 !important;
@@ -31,10 +149,48 @@
 </style>
 
 <script>
+import Vue from "vue";
+
 export default {
 	name: "NeoSettingsPage",
+	data() {
+		return {
+			demoPermission: "inherit",
+			demoPermission2: "deny"
+		}
+	},
 	mounted() {
 		this.$refs["settingsView"].navigate("server_settings");
+	},
+	methods: {
+		async addGroup() {
+			var addGroupDialog = new metroUI.ContentDialog("Add a new Group",
+			(() => {
+				return (
+					<div>
+						<p>Enter the name of the new group:</p>
+						<input type="text" />
+						
+						<p>Select a group to inherit from:</p>
+						<metro-combo-box>
+							<select>
+								{[{id:"id",name:"%group_name%"}].map(item => {
+									return (
+										<option value={item.id}>{item.name}</option>
+									)
+								})}
+							</select>
+						</metro-combo-box>
+					</div>
+				)
+			})(),
+			[{text: "Create Group", primary: true},{text: "Cancel"}]);
+			var result = await addGroupDialog.showAsync();
+			
+			if (result == 1) {
+				console.log(addGroupDialog.text);
+			}
+		}
 	}
 }
 </script>
