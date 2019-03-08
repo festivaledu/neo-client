2f7c864d62a031a975338d588de9519fcbaf2234
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-03-08 11:50:14 +0100

    Add sort value to group settings

--- src/components/NeoSettingsPage.vue
+++ src/components/NeoSettingsPage.vue
@@ -72,10 +72,13 @@
                         </template>
 
                         <h4>Allgemein</h4>
-                        <p>Gruppen-ID</p>
+                        <p class="text-label">Gruppen-ID</p>
                         <input type="text" v-model="group.id" />
-                        <p>Name</p>
+                        <p class="text-label">Name</p>
                         <input type="text" v-model="group.name" />
+                        <p class="text-label">Wertigkeit</p>
+                        <p class="detail-text-label">Die Wertigkeit bestimmt, in welcher Reihenfolge die Gruppen sortiert und die Rechte vererbt werden.<br />Eine Gruppe erbt immer von allen Gruppen mit niedrigerer Wertigkeit.</p>
+                        <input type="text" v-model="group.sortValue" />
 
 						{{ group }}
 					</div>
